62db9f6360e6753be30f5877c0bdb295453c4412
Shristy Thapa <thapashristy110@gmail.com>
2024-12-28 05:11:08 +0545

    icon change

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../assets/styles/Timeline.scss'
@@ -18,7 +18,7 @@ function Timeline() {
             contentArrowStyle={{ borderRight: '7px solid  white' }}
             date="2021 - 2024"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
             <p className="vertical-timeline-element-title">Softwarica College | Coventry University</p>
             <h4 className="vertical-timeline-element-subtitle">Bachelor of Science Hons in Computing </h4>
@@ -27,7 +27,7 @@ function Timeline() {
             className="vertical-timeline-element--work"
             date="Finished on 2021"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
             <p className="vertical-timeline-element-title">Rehdon Collage</p>
             <h4 className="vertical-timeline-element-subtitle">HighSchool (Management) </h4>
