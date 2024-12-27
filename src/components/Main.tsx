293924ae3710f7e86efff6590bd3ad7506ec8691
Shristy Thapa <thapashristy110@gmail.com>
2024-12-27 17:04:42 +0545

    projects added

---
+++ src/components/Main.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+  <div className="left-section">
+    <div className="about-section">
+      <div className="image-wrapper">
+        <img
+          src="https://res.cloudinary.com/duhlo06nb/image/upload/v1732186050/Snapchat-1185139239-modified_bqofey.png"
+          alt="Avatar"
+        />
+      </div>
+      <div className="content">
+        <div className="social_icons">
+          <a href="https://github.com/Shristythapa" target="_blank" rel="noreferrer">
+            <GitHubIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/shristythapab7a268225" target="_blank" rel="noreferrer">
+            <LinkedInIcon />
+          </a>
+        </div>
+        <h1>Shristy Thapa</h1>
+        {/* <p>Full Stack Engineer</p> */}
+
+        <div className="mobile_social_icons">
+          <a href="https://github.com/Shristythapa" target="_blank" rel="noreferrer">
+            <GitHubIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/shristythapab7a268225" target="_blank" rel="noreferrer">
+            <LinkedInIcon />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="right-section ">
+    <h1>Skills</h1>
+    <p>Languages & Tools: Python, SQL, Tableau, Git, R</p> 
+    <p>Methodologies: Agile Development (Scrum)</p>
+    <p>Soft Skills: Critical Thinking, Quick Learning, Teamwork, Communication, Leadership (Scrum Master experience)</p>
+  </div>
+</div>
+
+  );
+}
+
+export default Main;
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../assets/styles/Timeline.scss'
+
+function Timeline() {
+  return (
+    <div id="history">
+      <div className="items-container">
+        <h1>Education</h1>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="2021 - 2024"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Softwarica Collage</h3>
+            <h4 className="vertical-timeline-element-subtitle">Bachlors</h4>
+            <p>
+              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Finished on 2021"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Rehdon Collage</h3>
+            <h4 className="vertical-timeline-element-subtitle">HighSchool</h4>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
